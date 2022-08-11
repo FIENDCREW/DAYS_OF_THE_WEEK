6f41cbadf4dee32e7725488aba2856a1e693e0f3
FIENDCREW <odysseybmx911@gmail.com>
2022-08-11 14:20:13 +0300

    ready

---
+++ index.js
@@ -0,0 +1,35 @@
+const week = {
+  monday: 'MONDAY',
+  tuesday: 'TUESDAY',
+  wednesday: 'WEDNESDAY',
+  thursday: 'THURSDAY',
+  friday: 'FRIDAY',
+  saturday: 'SATURDAY',
+  sunday: 'SUNDAY',
+};
+
+let str = `Старший братец ${week.monday} –
+работяга, не бездельник.
+Он неделю открывает
+всех трудиться зазывает.
+
+${week.tuesday} следует за братом
+у него идей богато.
+
+А потом ${week.wednesday}-сестрица,
+не пристало ей лениться.
+
+Брат ${week.thursday} и так, и сяк,
+он мечтательный чудак.
+
+${week.friday}-сестра сумела
+побыстрей закончить дело.
+
+Предпоследний брат ${week.saturday}
+не выходит на работу.
+
+В гости ходит ${week.sunday},
+очень любит угощенье
+`;
+
+console.log(str);
